4ab300ee438a25716696516212f5817f1485f067
Sean Wheeler <sean.wheeler@microsoft.com>
2026-02-24 09:53:56 -0600

    Update Test-YamlTOC

--- Projects/Modules/Documentarian.MicrosoftDocs/Source/Public/Functions/Test-YamlTOC.ps1
+++ Projects/Modules/Documentarian.MicrosoftDocs/Source/Public/Functions/Test-YamlTOC.ps1
@@ -6,31 +6,59 @@ function Test-YamlTOC {
     [CmdletBinding()]
     param(
         [Parameter(Mandatory, Position = 0)]
+        [ValidateScript({ Test-Path $_ -PathType Container })]
         [string]$Path
     )
 
-    $toc = Get-Item $Path
-    $basepath = "$($toc.Directory)\"
+    $Path = (Resolve-Path $Path).Path
 
-    $hrefs = (Select-String -Pattern '\s*href:\s+([\w\-\._/]+)\s*$' -Path $toc.FullName).Matches |
-        ForEach-Object { $_.Groups[1].Value } |
-        Sort-Object
+    # Find the TOC files
+    $tocList = Get-ChildItem $Path -Filter 'toc.yml' -Recurse -File |
+        Where-Object { $_.FullName -notmatch 'bread' }
 
-    $hrefs | ForEach-Object {
-        $file = $basepath + ($_ -replace '/', '\')
-        if (-not (Test-Path $file)) {
-            "File does not exist - $_"
+    if ($tocList.Count -eq 0) {
+        Write-Warning "No TOC.yml files found in path: $Path"
+        return
+    }
+
+    # Find all content files
+    $fileList = Get-ChildItem $Path -Include *.md, *.yml -Recurse -File |
+        Where-Object { $_.FullName -notmatch 'bread.+\\toc.yml' }
+
+    $statusList = foreach ($file in $fileList) {
+        [pscustomobject]@{
+            FileExists  = $true
+            IsInTOC     = $false
+            FileName    = $file.FullName
+            TOCFileName = ''
         }
     }
 
-    $files = Get-ChildItem $basepath\*.md, $basepath\*.yml -Recurse -File |
-        Where-Object Name -NE 'toc.yml'
+    #Process TOC files
+    $hrefPattern = '\s*href:\s+([\w\-\._/]+)\s*$'
+    foreach ($toc in $tocList) {
+        $tocBasePath = Split-Path $toc.FullName -Parent
+        $hrefList = (Select-String -Pattern $hrefPattern -Path $toc.FullName).Matches |
+            ForEach-Object { $_.Groups[1].Value }
 
-    $files.FullName | ForEach-Object {
-        $file = ($_ -replace [regex]::Escape($basepath)) -replace '\\', '/'
-        if ($hrefs -notcontains $file) {
-            "File not in TOC - $file"
+        foreach ($href in $hrefList) {
+            $filePath = Join-Path $tocBasePath $href
+            if ($filePath -in $fileList.FullName) {
+                $statusList.Where({ $_.FileName -eq $filePath }).ForEach({
+                    $_.IsInTOC = $true
+                    $_.TOCFileName = $toc.FullName
+                })
+            } else {
+                $statusList += [pscustomobject]@{
+                    FileExists  = $false
+                    IsInTOC     = $true
+                    FileName    = $filePath
+                    TOCFileName = $toc.FullName
+                }
+            }
         }
     }
 
+    # Output results
+    $statusList | Where-Object { -not $_.FileExists -or -not $_.IsInTOC }
 }
